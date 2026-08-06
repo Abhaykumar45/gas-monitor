92abdbc43b9f5d177450ffd78192f4e5116bad5d
Abhaykumar45 <abhaykumargkp32@gmail.com>
2026-08-06 14:45:14 +0530

    alexa relay control

--- controllers/alexaController.js
+++ controllers/alexaController.js
@@ -1,31 +1,52 @@
+const Device = require("../models/Device");
+const { publishCommand } = require("../services/mqttPublisher");
+
 exports.relayControl = async (req, res) => {
 
     try {
 
-        const {
-            deviceName,
-            action,
-        } = req.body;
+        const { deviceName, action } = req.body;
 
-        console.log("Alexa Request");
-        console.log(deviceName);
-        console.log(action);
+        const device = await Device.findOne({
+            deviceName: new RegExp(`^${deviceName}$`, "i")
+        });
+
+        if (!device) {
+
+            return res.json({
+                success: false,
+                speech: `I couldn't find a device named ${deviceName}`
+            });
+
+        }
+
+        publishCommand(device.deviceId, {
+
+            relay: action === "on" ? 1 : 0,
+
+            valve: device.valve,
+
+            mode: device.mode
+
+        });
 
         return res.json({
 
             success: true,
 
-            speech: `${deviceName} relay ${action}`
+            speech: `${device.deviceName} relay turned ${action}`
 
         });
 
     } catch (err) {
 
-        res.status(500).json({
+        console.log(err);
+
+        return res.status(500).json({
 
             success: false,
 
-            speech: "Something went wrong"
+            speech: "Something went wrong."
 
         });
 
