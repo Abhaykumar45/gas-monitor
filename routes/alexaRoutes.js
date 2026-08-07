067a290aaa640b35653c3999832ad900d621ad2c
Abhaykumar45 <abhaykumargkp32@gmail.com>
2026-08-07 15:03:44 +0530

    Add OAuth account 9

--- routes/alexaRoutes.js
+++ routes/alexaRoutes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const {
-    relayControl,
+    relayControl,getGasStatus
 } = require("../controllers/alexaController");
 
 router.post("/relay", relayControl);
